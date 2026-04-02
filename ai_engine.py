65daf4999bf627524d56da3cd6ed98f2a08f670d
Pushkar Chauhan <pushkarchauhan138@gmail.com>
2026-04-02 13:13:43 +0530

    Update ai_engine.py

--- ai_engine.py
+++ ai_engine.py
@@ -1,17 +1,18 @@
 import requests
-
-API_KEY = "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"
-
+import streamlit as st
+API_KEY = st.secrets["OPENROUTER_API_KEY"]
 def call_ai(prompt):
     url = "https://openrouter.ai/api/v1/chat/completions"
 
     headers = {
-        "Authorization": f"Bearer {API_KEY}",
-        "Content-Type": "application/json"
-    }
+    "Authorization": f"Bearer {API_KEY}",
+    "Content-Type": "application/json",
+    "HTTP-Referer": "https://learnova-ai-g7tb537n5ebipcvuphtydg.streamlit.app",
+    "X-Title": "Learnova"
+}
 
     data = {
-        "model": "openai/gpt-3.5-turbo",
+        "model": "mistralai/mistral-7b-instruct",
         "messages": [{"role": "user", "content": prompt}]
     }
 
